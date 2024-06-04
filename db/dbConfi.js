165110de3b801747ba7bc1d130dce288a2b78883
EffahAdode3 <whatyousow00@gmail.com>
2024-06-04 05:49:53 -0700

    add another breach to index

--- db/dbConfi.js
+++ db/dbConfi.js
@@ -10,7 +10,7 @@ const Sequelize = new sequelize(
      {
         host: process.env.Host || 'localhost',
         port: process.env.Port || 3306,
-        dialect: 'mysql', // Specify the dialect explicitly
+        dialect: 'mysql', 
         dialectModule: mysql2,
         logging: false
 });
