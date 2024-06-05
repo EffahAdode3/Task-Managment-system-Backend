4f37868da368677ede1d71ca71163ddc91033baa
EffahAdode3 <whatyousow00@gmail.com>
2024-06-05 16:54:32 -0700

    New push to the backend

--- model/TodoList.js
+++ model/TodoList.js
@@ -1,20 +1,16 @@
-// Import Sequelize and necessary modules
-import sequelize from "../db/dbConfig.js";
+import sequelize from "../db/dbConfi.js";
 import DataType from "sequelize";
 import Client from "./clientModel.js";
 
-// Function to format date into a string representation
 const formatDate = (date) => {
   const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
   return `${daysOfWeek[date.getUTCDay()]} ${months[date.getUTCMonth()]} ${date.getUTCDate()} ${date.getUTCFullYear()}`;
 };
 
-// Define the TodoList model
 const TodoList = sequelize.define(
   "TodoList",
   {
-    // Define fields for the TodoList model
     id: {
       type: DataType.INTEGER,
       autoIncrement: true,
@@ -33,27 +29,28 @@ const TodoList = sequelize.define(
       type: DataType.STRING,
       allowNull: false,
     },
+    formattedCreatedAt: {
+      type: DataType.STRING,
+      allowNull: true,
+    },
+    formattedUpdatedAt: {
+      type: DataType.STRING,
+      allowNull: true,
+    },
   },
-  // Include timestamps in the model
   { timestamps: true }
 );
 
-// Hook to format timestamps before creating a TodoList record
 TodoList.beforeCreate((todo) => {
-  // Format createdAt and updatedAt timestamps
-  todo.createdAt = formatDate(new Date(todo.createdAt));
-  todo.updatedAt = formatDate(new Date(todo.updatedAt));
+  todo.formattedCreatedAt = formatDate(new Date(todo.createdAt));
+  todo.formattedUpdatedAt = formatDate(new Date(todo.updatedAt));
 });
 
-// Hook to format updatedAt timestamp before updating a TodoList record
 TodoList.beforeUpdate((todo) => {
-  // Format updatedAt timestamp
-  todo.updatedAt = formatDate(new Date(todo.updatedAt));
+  todo.formattedUpdatedAt = formatDate(new Date(todo.updatedAt));
 });
 
-// Establish a relationship between TodoList and Client models
 TodoList.belongsTo(Client, { foreignKey: 'client_id' });
 Client.hasMany(TodoList, { foreignKey: 'client_id' });
 
-// Export the TodoList model as the default
 export default TodoList;
