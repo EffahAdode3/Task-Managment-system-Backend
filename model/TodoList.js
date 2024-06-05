b640f2d987f968b9704c58ea471472a4172ab6eb
EffahAdode3 <whatyousow00@gmail.com>
2024-06-05 16:36:53 -0700

    New push

--- model/TodoList.js
+++ model/TodoList.js
@@ -2,6 +2,13 @@ import sequelize from "../db/dbConfi.js";
 import DataType from "sequelize";
 import Client from "./clientModel.js";
 
+function formatDate(dateString) {
+    const date = new Date(dateString);
+    const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    return daysOfWeek[date.getUTCDay()] + " " + months[date.getUTCMonth()] + " " + date.getUTCDate() + " " + date.getUTCFullYear();
+}
+
 const TodoList = sequelize.define(
   "TodoList",
   {
@@ -24,10 +31,21 @@ const TodoList = sequelize.define(
       allowNull: false,
     }, 
   },
-  { timestamps: true }
+  { 
+    timestamps: true,
+    hooks: {
+      beforeCreate: (todo) => {
+        todo.createdAt = formatDate(todo.createdAt);
+        todo.updatedAt = formatDate(todo.updatedAt);
+      },
+      beforeUpdate: (todo) => {
+        todo.updatedAt = formatDate(todo.updatedAt);
+      }
+    }
+  }
 );
 
-TodoList.belongsTo(Client,{foreignKey: 'client_id'});
-Client.hasMany(TodoList,{foreignKey:'client_id'});
+TodoList.belongsTo(Client, { foreignKey: 'client_id' });
+Client.hasMany(TodoList, { foreignKey: 'client_id' });
 
 export default TodoList;
