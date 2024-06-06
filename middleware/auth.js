c473ada8fc06dbca9ea77d0487aa9d72a81f3f55
EffahAdode3 <whatyousow00@gmail.com>
2024-06-06 13:37:14 -0700

    New push to the Local Storage

--- middleware/auth.js
+++ middleware/auth.js
@@ -65,5 +65,4 @@ const tokenVerification = async (req, res, next) => {
       return res.status(500).json({ message: "Error", error });
     }
   };
-  
 export default {generateToken, tokenVerification }
