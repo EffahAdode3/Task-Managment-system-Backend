8e1a7212171f23484dd65bea3a3e2aeb38687d9b
EffahAdode3 <whatyousow00@gmail.com>
2024-06-06 12:58:35 -0700

    New push to the Local Storage

--- middleware/auth.js
+++ middleware/auth.js
@@ -26,7 +26,7 @@ const generateToken = async(req, res, next) =>{
     email:findClient.email,
     userName:findClient.userName,   
  }
-const ClientToken = jwt.sign(generatetoken, secret, { expiresIn: "1m" });
+const ClientToken = jwt.sign(generatetoken, secret, { expiresIn: "60s" });
  req.token = ClientToken
  req.user = findClient;
 
