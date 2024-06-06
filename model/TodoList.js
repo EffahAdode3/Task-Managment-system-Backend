ac9e70124d1040157a15a59a920fb147918088b9
EffahAdode3 <whatyousow00@gmail.com>
2024-06-06 03:54:03 -0700

    New push to the backend

--- model/TodoList.js
+++ model/TodoList.js
@@ -2,11 +2,12 @@ import sequelize from "../db/dbConfi.js";
 import DataType from "sequelize";
 import Client from "./clientModel.js";
 
-const formatDate = (date) => {
-  const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-  return `${daysOfWeek[date.getUTCDay()]} ${months[date.getUTCMonth()]} ${date.getUTCDate()} ${date.getUTCFullYear()}`;
-};
+function formatDate(dateString) {
+    const date = new Date(dateString);
+    const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    return daysOfWeek[date.getUTCDay()] + " " + months[date.getUTCMonth()] + " " + date.getUTCDate() + " " + date.getUTCFullYear();
+}
 
 const TodoList = sequelize.define(
   "TodoList",
@@ -20,7 +21,7 @@ const TodoList = sequelize.define(
     newTodo: {
       type: DataType.STRING,
       allowNull: false,
-    },
+    }, 
     category: {
       type: DataType.STRING,
       allowNull: false,
@@ -28,28 +29,21 @@ const TodoList = sequelize.define(
     deadline: {
       type: DataType.STRING,
       allowNull: false,
-    },
-    formattedCreatedAt: {
-      type: DataType.STRING,
-      allowNull: true,
-    },
-    formattedUpdatedAt: {
-      type: DataType.STRING,
-      allowNull: true,
-    },
+    }, 
   },
-  { timestamps: true }
+  { 
+    timestamps: true,
+    hooks: {
+      beforeCreate: (todo) => {
+        todo.createdAt = formatDate(todo.createdAt);
+        todo.updatedAt = formatDate(todo.updatedAt);
+      },
+      beforeUpdate: (todo) => {
+        todo.updatedAt = formatDate(todo.updatedAt);
+      }
+    }
+  }
 );
-
-TodoList.beforeCreate((todo) => {
-  const now = new Date();
-  todo.formattedCreatedAt = formatDate(now);
-  todo.formattedUpdatedAt = formatDate(now);
-});
-
-TodoList.beforeUpdate((todo) => {
-  todo.formattedUpdatedAt = formatDate(new Date());
-});
 
 TodoList.belongsTo(Client, { foreignKey: 'client_id' });
 Client.hasMany(TodoList, { foreignKey: 'client_id' });
