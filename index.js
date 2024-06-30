7d7194964175fbf52713ac84586235018cce3cee
EffahAdode3 <whatyousow00@gmail.com>
2024-06-30 15:51:17 +0000

    service added

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ import express from "express"
 import bodyParser from "body-parser"
 import  sequelize  from "./db/dbConfi.js"
 import cors from 'cors'
-import clientRoutes from './routes/clientRoutes.js'
+import Routes from './routes/clientRoutes.js'
 import dotenv from "dotenv";
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 dotenv.config();
 const port = process.env.Port;
-app.use('/', clientRoutes);
+app.use('/', Routes);
 try {
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
