a67a988c729436b8721a8f51ee78c7fff88f229b
EffahAdode3 <whatyousow00@gmail.com>
2024-07-29 10:19:46 +0000

    Client data

--- utils/uploadExcel.js
+++ utils/uploadExcel.js
@@ -14,7 +14,7 @@ const uploadExcel = async(req,res)=>{
         }
         return res.status(200).json({message:"success"})
     }catch(error){
-        console.log(error)
+        console.error(error)
     }
 }
 
