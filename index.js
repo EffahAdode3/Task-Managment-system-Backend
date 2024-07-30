128fbbbe6ed0f2f59f126adee0132a006794aaa1
EffahAdode3 <whatyousow00@gmail.com>
2024-07-30 15:20:57 +0000

    Doc upload

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ import express from "express"
 import bodyParser from "body-parser"
 import  sequelize  from "./db/dbConfi.js"
 import cors from 'cors'
-import path from 'path'
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+// import path from 'path'
+// import { fileURLToPath } from 'url';
+// import { dirname } from 'path';
 import Routes from './routes/clientRoutes.js'
 import dotenv from "dotenv";
 const app = express();
 app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: false }));
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-app.use( express.static(path.join(__dirname, 'uploadedDoc')));
+// app.use(express.urlencoded({ extended: false }));
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+// app.use( express.static(path.join(__dirname, 'uploadedDoc')));
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 dotenv.config();
