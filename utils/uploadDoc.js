41ba028b17be286806889fc1c89207692b068afb
EffahAdode3 <whatyousow00@gmail.com>
2024-07-30 15:48:26 +0000

    some message here

--- utils/uploadDoc.js
+++ utils/uploadDoc.js
@@ -3,7 +3,7 @@ import path from 'path'
 
 const stortage = multer.diskStorage({
     destination:(req,file,cb)=>{
-        cb(null, '../uploadedDoc')
+        cb(null, './uploadedDoc')
     },
     filename:(req, file, cb) =>{
        cb(null, Date.now() + path.extname(file.originalname))
