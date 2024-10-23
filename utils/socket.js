b90fca3bc8172a81002222479e3f06df9f7bac61
EffahAdode3 <whatyousow00@gmail.com>
2024-10-23 10:31:15 +0000

    new chat

--- utils/socket.js
+++ utils/socket.js
@@ -1,93 +1,29 @@
 
 import Message  from '../model/message.js'; 
-// export const socketHandler = (io) => {
-//   // Handle new socket connection
-//   io.on('connection', (socket) => {
-//     console.log('A user connected');
-
-//     // Join chat event when a user connects
-//     socket.on('joinChat', (email) => {
-//       console.log(`${email} joined the chat`);
-//     });
-
-//     // Handle sending messages
-//     socket.on('sendMessage', async (data) => {
-//       const { senderId, receiverId, message, fromEmail } = data;
-
-//       // Save the message to the database
-//       await Message.create({ senderId, receiverId, message });
-
-//       // Emit the message to the receiver
-//       io.emit('receiveMessage', { fromEmail, message });
-//     });
-
-//     // Handle disconnection
-//     socket.on('disconnect', () => {
-//       console.log('User disconnected');
-//     });
-//   });
-// };
 export const socketHandler = (io) => {
-  const users = {}; // Store connected users by their email
-
+  // Handle new socket connection
   io.on('connection', (socket) => {
-    console.log('A user connected:', socket.id);
+    console.log('A user connected');
 
-    // Store user's socket id by email when they join
+    // Join chat event when a user connects
     socket.on('joinChat', (email) => {
-      users[email] = socket.id;
       console.log(`${email} joined the chat`);
     });
 
-    // Handle live stream invite
-    socket.on('inviteToStream', (data) => {
-      const { fromEmail, toEmail } = data;
-      const receiverSocketId = users[toEmail];
-      if (receiverSocketId) {
-        io.to(receiverSocketId).emit('receiveInvite', { fromEmail });
-        console.log(`Invitation sent from ${fromEmail} to ${toEmail}`);
-      }
-    });
+    // Handle sending messages
+    socket.on('sendMessage', async (data) => {
+      const { senderId, receiverId, message, fromEmail } = data;
 
-    // Handle live stream acceptance
-    socket.on('acceptInvite', (data) => {
-      const { fromEmail, toEmail } = data;
-      const senderSocketId = users[fromEmail];
-      if (senderSocketId) {
-        io.to(senderSocketId).emit('inviteAccepted', { toEmail });
-        console.log(`${toEmail} accepted the invite from ${fromEmail}`);
-      }
-    });
+      // Save the message to the database
+      await Message.create({ senderId, receiverId, message });
 
-    // Handle stream offer
-    socket.on('streamOffer', (data) => {
-      const { offer, toEmail, fromEmail } = data;
-      const receiverSocketId = users[toEmail];
-      if (receiverSocketId) {
-        io.to(receiverSocketId).emit('streamOffer', { offer, fromEmail });
-        console.log(`Stream offer sent from ${fromEmail} to ${toEmail}`);
-      }
-    });
-
-    // Handle stream answer
-    socket.on('streamAnswer', (data) => {
-      const { answer, toEmail, fromEmail } = data;
-      const receiverSocketId = users[toEmail];
-      if (receiverSocketId) {
-        io.to(receiverSocketId).emit('streamAnswer', { answer, fromEmail });
-        console.log(`Stream answer sent from ${fromEmail} to ${toEmail}`);
-      }
+      // Emit the message to the receiver
+      io.emit('receiveMessage', { fromEmail, message });
     });
 
     // Handle disconnection
     socket.on('disconnect', () => {
-      console.log('User disconnected:', socket.id);
-      for (let email in users) {
-        if (users[email] === socket.id) {
-          delete users[email];
-          break;
-        }
-      }
+      console.log('User disconnected');
     });
   });
 };
