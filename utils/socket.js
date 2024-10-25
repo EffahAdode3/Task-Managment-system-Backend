838252924a80e8eb3993c1db8d01dec01b782828
EffahAdode3 <whatyousow00@gmail.com>
2024-10-25 03:28:21 +0000

    new message

--- utils/socket.js
+++ utils/socket.js
@@ -1,5 +1,36 @@
 
-import Message  from '../model/message.js'; 
+// import Message  from '../model/message.js'; 
+// export const socketHandler = (io) => {
+//   // Handle new socket connection
+//   io.on('connection', (socket) => {
+//     console.log('A user connected');
+
+//     // Join chat event when a user connects
+//     socket.on('joinChat', (email) => {
+//       console.log(`${email} joined the chat`);
+//     });
+
+//     // Handle sending messages
+//     socket.on('sendMessage', async (data) => {
+//       const { senderId, receiverId, message, fromEmail } = data;
+
+//       // Save the message to the database
+//       await Message.create({ senderId, receiverId, message });
+
+//       // Emit the message to the receiver
+//       io.emit('receiveMessage', { fromEmail, message });
+//     });
+
+//     // Handle disconnection
+//     socket.on('disconnect', () => {
+//       console.log('User disconnected');
+//     });
+//   });
+// };
+
+
+import Message from '../model/message.js'; 
+
 export const socketHandler = (io) => {
   // Handle new socket connection
   io.on('connection', (socket) => {
@@ -17,8 +48,8 @@ export const socketHandler = (io) => {
       // Save the message to the database
       await Message.create({ senderId, receiverId, message });
 
-      // Emit the message to the receiver
-      io.emit('receiveMessage', { fromEmail, message });
+      // Emit the message to the receiver (broadcast to others except the sender)
+      socket.broadcast.emit('receiveMessage', { fromEmail, message });
     });
 
     // Handle disconnection
